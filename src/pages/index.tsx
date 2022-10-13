0b6acb73ed6307edbe29415c29c48b09e6ec8514
nicolas giraudo <giraudo.nicolas13@gmail.com>
2022-10-13 19:58:41 +0400

    feat: init work homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,5 @@
-import { useEffect, useState } from 'react';
-
 const HomePage = () => {
-  const [enabled, enable] = useState(false);
-  useEffect(() => {
-    enable(true);
-  }, []);
-
-  return (
-    <>
-      <p className="bg-red-500 border border-solid">TODO</p>
-      {enabled && <p>TEMP</p>}
-    </>
-  );
+  return <p className="bg-red-500 border border-solid">TODO</p>;
 };
 
 export default HomePage;
