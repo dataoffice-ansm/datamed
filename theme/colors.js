d253b2f2c8f9f237063a14cc3a09f45006de9020
Alexandre <sensory_upshot_0e@icloud.com>
2022-10-20 11:19:54 +0200

    feat(theme): add color palette
    Add color palette on tailwind config
    
    ISSUES CLOSED #DATS-210

---
+++ theme/colors.js
@@ -0,0 +1,339 @@
+const darkViolet = {
+  50: '#EFEAF3',
+  100: '#DFD4E5',
+  200: '#CFC0D9',
+  300: '#BFAACB',
+  400: '#AE94BE',
+  500: '#9E7FB2',
+  600: '#8F6AA5',
+  700: '#7E5598',
+  800: '#6F408B',
+  900: '#5E2A7E',
+  DEFAULT: '#5E2A7E',
+};
+
+const lightViolet = {
+  50: '#F6EBF4',
+  100: '#ECD6E7',
+  200: '#E3C2DC',
+  300: '#D9ADD0',
+  400: '#CF97C3',
+  500: '#C683B8',
+  600: '#BD6FAD',
+  700: '#B35AA1',
+  800: '#AA4695',
+  900: '#A03189',
+  DEFAULT: '#A03189',
+};
+
+const orange = {
+  50: '#FEF2E6',
+  100: '#FCE5CC',
+  200: '#FBD8B3',
+  300: '#F9CB99',
+  400: '#F6BD7F',
+  500: '#F5B166',
+  600: '#F4A44D',
+  700: '#F29733',
+  800: '#F18A1A',
+  900: '#EF7D00',
+  DEFAULT: '#EF7D00',
+};
+
+const red = {
+  50: '#FDE6ED',
+  100: '#FACCDA',
+  200: '#F8B3C8',
+  300: '#F599B5',
+  400: '#F17FA2',
+  500: '#EF6690',
+  600: '#ED4D7E',
+  700: '#EA336B',
+  800: '#E81A59',
+  900: '#E50046',
+  DEFAULT: '#E50046',
+};
+
+const turquoise = {
+  50: '#E6F8FA',
+  100: '#CCF0F5',
+  200: '#B3E9F0',
+  300: '#99E1EB',
+  400: '#7FD8E5',
+  500: '#66D1E0',
+  600: '#4DCADC',
+  700: '#33C2D6',
+  800: '#1ABBD2',
+  900: '#00B3CC',
+  DEFAULT: '#00B3CC',
+};
+
+const green = {
+  50: '#F7FAE7',
+  100: '#EFF4CE',
+  200: '#E7F0B6',
+  300: '#DFEA9D',
+  400: '#D6E484',
+  500: '#CFDF6D',
+  600: '#C7DA55',
+  700: '#BFD43C',
+  800: '#B7D024',
+  900: '#AFCA0B',
+  DEFAULT: '#AFCA0B',
+};
+
+const darkGreen = {
+  50: '#E6F5EC',
+  100: '#CCEAD9',
+  200: '#B3E0C6',
+  300: '#99D5B3',
+  400: '#7FCA9F',
+  500: '#66C08C',
+  600: '#4DB67A',
+  700: '#33AB66',
+  800: '#1AA154',
+  900: '#009640',
+  DEFAULT: '#009640',
+};
+
+const grey = {
+  10: '#FCFCFC',
+  50: '#F2F2F2',
+  100: '#E6E6E6',
+  200: '#CCCCCC',
+  300: '#B3B3B3',
+  400: '#999999',
+  500: '#7F7F7F',
+  600: '#666666',
+  700: '#4D4D4D',
+  800: '#333333',
+  900: '#1A1A1A',
+  DEFAULT: '#1A1A1A',
+};
+
+const darkBlue = {
+  50: '#EAEBF3',
+  100: '#D4D7E5',
+  200: '#BFC3D9',
+  300: '#A9AFCC',
+  400: '#939ABE',
+  500: '#7E86B2',
+  600: '#6973A6',
+  700: '#535E99',
+  800: '#3E4B8C',
+  900: '#28367F',
+  DEFAULT: '#28367F',
+};
+
+const blue = {
+  20: '#FAFCFE',
+  50: '#E7F1F8',
+  100: '#D0E3F1',
+  200: '#B8D6EB',
+  300: '#A0C8E4',
+  400: '#88B9DC',
+  500: '#71ACD6',
+  600: '#5A9FD0',
+  700: '#4191C9',
+  800: '#2A83C2',
+  900: '#1275BB',
+  DEFAULT: '#1275BB',
+};
+
+const darkYellow = {
+  50: '#FFF8E6',
+  100: '#FEEFCC',
+  200: '#FEE8B3',
+  300: '#FDDF99',
+  400: '#FBD77F',
+  500: '#FBD066',
+  600: '#FBC84D',
+  700: '#FAC033',
+  800: '#FAB81A',
+  900: '#F9B000',
+  DEFAULT: '#F9B000',
+};
+
+const yellow = {
+  50: '#FFFCE6',
+  100: '#FFF8CC',
+  200: '#FFF6B3',
+  300: '#FFF299',
+  400: '#FFEE7F',
+  500: '#FFEB66',
+  600: '#FFE84D',
+  700: '#FFE533',
+  800: '#FFE21A',
+  900: '#FFDE00',
+  DEFAULT: '#FFDE00',
+};
+
+const darkRed = {
+  50: '#F7E8ED',
+  100: '#EED0DA',
+  200: '#E6B9C8',
+  300: '#DDA1B5',
+  400: '#D488A2',
+  500: '#CC7190',
+  600: '#C45A7E',
+  700: '#BB426B',
+  800: '#B32B59',
+  900: '#AA1346',
+  DEFAULT: '#AA1346',
+};
+
+const pink = {
+  50: '#FCEDF4',
+  100: '#F8D9E9',
+  200: '#F4C7DE',
+  300: '#F0B3D3',
+  400: '#ECA0C7',
+  500: '#E98EBC',
+  600: '#E67BB2',
+  700: '#E168A6',
+  800: '#DE559B',
+  900: '#DA4290',
+  DEFAULT: '#DA4290',
+};
+
+const teal = {
+  50: '#E6F5F5',
+  100: '#CCEBE9',
+  200: '#B3E1DF',
+  300: '#99D7D4',
+  400: '#7FCCC8',
+  500: '#66C2BE',
+  600: '#4DB9B4',
+  700: '#33AEA9',
+  800: '#1AA59E',
+  900: '#009A93',
+  DEFAULT: '#009A93',
+};
+
+const mint = {
+  50: '#F6FBF7',
+  100: '#ECF6EF',
+  200: '#E3F2E7',
+  300: '#D9EDDF',
+  400: '#CFE7D6',
+  500: '#C6E3CF',
+  600: '#BDDFC7',
+  700: '#B3DABF',
+  800: '#AAD6B7',
+  900: '#A0D1AF',
+  DEFAULT: '#A0D1AF',
+};
+
+const skin1 = {
+  50: '#FFFFFF',
+  100: '#FFF8EE',
+  200: '#FFF5E7',
+  300: '#FFF1DE',
+  400: '#FFECD5',
+  500: '#FFE9CD',
+  600: '#FFE6C5',
+  700: '#FFE2BD',
+  800: '#FFDFB5',
+  900: '#FFDBAC',
+  DEFAULT: '#FFDBAC',
+};
+
+const skin2 = {
+  50: '#FEF9F2',
+  100: '#FCF3E5',
+  200: '#FBEDD8',
+  300: '#F9E7CB',
+  400: '#F7E0BD',
+  500: '#F7DAB1',
+  600: '#F6D5A4',
+  700: '#F4CE97',
+  800: '#F3C98A',
+  900: '#F1C27D',
+  DEFAULT: '#F1C27D',
+};
+
+const skin3 = {
+  50: '#FCF7F0',
+  100: '#F9EEE1',
+  200: '#F6E7D2',
+  300: '#F3DEC3',
+  400: '#EFD5B3',
+  500: '#ECCDA5',
+  600: '#EAC596',
+  700: '#E6BD87',
+  800: '#E4B578',
+  900: '#E0AC69',
+  DEFAULT: '#E0AC69',
+};
+
+const skin4 = {
+  50: '#FAF3ED',
+  100: '#F4E7D9',
+  200: '#EEDBC7',
+  300: '#E8CFB3',
+  400: '#E2C2A0',
+  500: '#DDB68E',
+  600: '#D8AB7B',
+  700: '#D19E68',
+  800: '#C68642',
+  900: '#C68642',
+  DEFAULT: '#C68642',
+};
+
+const skin5 = {
+  50: '#F4EEEA',
+  100: '#E8DDD3',
+  200: '#DDCCBE',
+  300: '#D1BBA7',
+  400: '#C5A991',
+  500: '#BB997C',
+  600: '#B08866',
+  700: '#A47750',
+  800: '#99663A',
+  900: '#8D5524',
+  DEFAULT: '#8D5524',
+};
+
+const skin6 = {
+  50: '#EEEBE7',
+  100: '#DDD5CE',
+  200: '#CCC1B7',
+  300: '#BBAB9E',
+  400: '#A99685',
+  500: '#99826D',
+  600: '#886D55',
+  700: '#77583D',
+  800: '#664325',
+  900: '#552E0C',
+  DEFAULT: '#552E0C',
+};
+
+const white = '#FFFFFF';
+const black = '#000000';
+
+module.exports = {
+  black,
+  white,
+  darkViolet,
+  lightViolet,
+  orange,
+  red,
+  turquoise,
+  green,
+  darkGreen,
+  grey,
+  darkBlue,
+  blue,
+  darkYellow,
+  yellow,
+  darkRed,
+  pink,
+  teal,
+  mint,
+  skin1,
+  skin2,
+  skin3,
+  skin4,
+  skin5,
+  skin6,
+};
