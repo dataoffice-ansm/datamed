fc7632f4d92096e935bb3f3e333b94f0833ff027
choucry13 <beroucry@yahoo.fr>
2022-10-20 16:18:32 +0200

    DATS-211 feat(tools): add commiltlint's rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,22 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['commitlint-plugin-function-rules'],
+  rules: {
+    'type-empty': [0],
+    'subject-empty': [0],
+    'type-enum': [0],
+    'function-rules/type-enum': [
+      2,
+      'always',
+      (parsed) => {
+        const headerRegex =
+          /^(DATS-\d+)|(build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test): (.+)$/;
+        const isHeaderValid = parsed.header.match(headerRegex);
+        if (isHeaderValid) {
+          return [true];
+        }
+        return [false, `header must match this regex: ${headerRegex}`];
+      },
+    ],
+  },
+};
