42d6dd47ffd7369eb9df01a7c640ce4c9d7cd956
nicolas giraudo <giraudo.nicolas13@gmail.com>
2022-10-28 10:00:52 +0400

    DATS-9 feat: improve Tooltip component

--- tests/components/Tooltip.spec.tsx
+++ tests/components/Tooltip.spec.tsx
@@ -5,30 +5,29 @@ describe(Tooltip.name, () => {
   const expectOnMouseEnter = async (resultToFind: string) => {
     fireEvent(screen.getByText('test'), new MouseEvent('mouseenter', { bubbles: true }));
     await waitFor(() => screen.getByRole('tooltip'));
-    expect(screen.getByRole('tooltip').className).toContain(resultToFind);
+    expect(screen.getByRole('tooltip').getAttribute('data-theme')).toBe(resultToFind);
     // The positioning does not matter
     cleanup();
   };
 
   test('should render', async () => {
     render(
-      <Tooltip content="content" title="title">
-        <span>test</span>
-      </Tooltip>
+      <Tooltip content="content" title="title" render={(refCb) => <span ref={refCb}>test</span>} />
     );
-
-    await expectOnMouseEnter('tooltip-container');
   });
 
   test.each<['white' | 'turquoise']>([['white'], ['turquoise']])(
     'should render %p theme',
     async (color: 'white' | 'turquoise') => {
       render(
-        <Tooltip content="content" title="title" theme={color}>
-          <span>test</span>
-        </Tooltip>
+        <Tooltip
+          content="content"
+          title="title"
+          theme={color}
+          render={(refCb) => <span ref={refCb}>test</span>}
+        />
       );
-      await expectOnMouseEnter(`bg-${color}`);
+      await expectOnMouseEnter(color);
     }
   );
 });
