7dca0429e708790989864baa167bd65295576a65
nicolas giraudo <giraudo.nicolas13@gmail.com>
2022-11-01 19:59:20 +0400

    DATS-164 fix: minor clean

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import { IntroductionSection } from '../components/Landing/IntroductionSection';
 import { MoreInformationSection } from '../components/Landing/MoreInformationSection';
 
 const HomePage = () => (
-  <div className="container">
+  <>
     <IntroductionSection />
     <SearchSection />
     <MoreInformationSection />
-  </div>
+  </>
 );
 
 export default HomePage;
