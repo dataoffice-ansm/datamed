c2c8494bf146fd74b6613e19383d0c1149ca410f
nicolas giraudo <giraudo.nicolas13@gmail.com>
2022-11-01 19:52:14 +0400

    DATS-78 feat: clean/improve page layout with sideMenu auto detection

---
+++ src/components/Layouts/Layout.tsx
@@ -0,0 +1,24 @@
+import { Footer } from '../Footer';
+import React from 'react';
+import { NavigationBar } from '../Navigation/NavigationBar';
+
+/**
+ *
+ * @param children
+ * @constructor
+ */
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+  // eslint-disable-next-line no-warning-comments
+  // TODO: en attente du context sur la headerHeight
+  const headerHeight = 100;
+
+  return (
+    <>
+      <NavigationBar />
+      <main style={{ paddingTop: `${headerHeight}px` }}>
+        <div className="md:container max-w-full md:mx-auto">{children}</div>
+      </main>
+      <Footer />
+    </>
+  );
+};
--- src/pages/cis/[cisId].tsx
+++ src/pages/cis/[cisId].tsx
@@ -1,3 +1,28 @@
-const PageCis = () => <p> TODO </p>;
+import SideMenuPageLayout from '../../components/Layouts/SideMenuPageLayout';
 
-export default PageCis;
+const PageSpeciality = () => (
+  <SideMenuPageLayout.Wrapper colorMenu="primary">
+    <SideMenuPageLayout.Item id="data-ansm-question" label="DATA.ANSM c'est quoi ?">
+      <div className="min-h-screen text-center">
+        <h1>Section 1</h1>
+      </div>
+    </SideMenuPageLayout.Item>
+
+    <SideMenuPageLayout.Item
+      id="donnees-globales-plateforme"
+      label="Données globales de la plateforme"
+    >
+      <div className="min-h-screen text-center">
+        <h1>Section 2</h1>
+      </div>
+    </SideMenuPageLayout.Item>
+
+    <SideMenuPageLayout.Item id="lecture-des-donnees" label="Lecture des données">
+      <div className="min-h-screen text-center">
+        <h1>Section 3</h1>
+      </div>
+    </SideMenuPageLayout.Item>
+  </SideMenuPageLayout.Wrapper>
+);
+
+export default PageSpeciality;
