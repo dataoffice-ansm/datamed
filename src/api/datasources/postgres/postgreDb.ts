a49ff2568c26203b9c054bfc2bda9eb970f03dec
nicolas giraudo <giraudo.nicolas13@gmail.com>
2022-11-30 17:46:53 +0400

    DATS-223 fix: resolve postgresDriver connection error

--- src/api/datasources/postgres/postgreDb.ts
+++ src/api/datasources/postgres/postgreDb.ts
@@ -4,9 +4,14 @@ import type { DB } from './schema.introspection';
 
 export const dbInstance = new Kysely<DB>({
   dialect: new PostgresDialect({
-    pool: new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-    }),
+    pool: async () =>
+      new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }),
   }),
+  log: [
+    //'query',
+    'error',
+  ],
 });
