60f6973efb13b7f8c333455fc504af82fced9974
choucry13 <beroucry@yahoo.fr>
2022-12-01 11:24:06 +0100

    DATS-237 feat(components): modify block more section

--- src/componentsPages/Landing/MoreInfoSection/MoreInfoSection.tsx
+++ src/componentsPages/Landing/MoreInfoSection/MoreInfoSection.tsx
@@ -5,7 +5,7 @@ import SickPersonSvg from '../../../assets/images/sick_person.svg';
 
 export const MoreInfoSection = () => (
   <div className="w-full flex justify-center items-center flex-col py-16">
-    <h2 className="py-4 text-2xl md:text-3xl font-medium">Données d’ensemble</h2>
+    <h2 className="py-4 text-2xl md:text-3xl font-medium">Statistiques globales</h2>
     <p className="max-w-2xl text-lg md:text-xl text-center mb-16">
       Envie d’en savoir plus sur les données qui constituent ce site ?<br />
       Consultez les déclarations reçues par l’ANSM pour l’ensemble des médicaments et substances
@@ -15,9 +15,8 @@ export const MoreInfoSection = () => (
     <div className="m-auto max-w-xl lg:max-w-6xl">
       <CardWithImage
         className="mb-8"
-        title="Ruptures de stock de médicaments"
-        description="Renseignez-vous sur l'historique des ruptures de stock des médicaments d'intérêt thérapeutique majeur, ainsi que les chiffres autour de la gestion des ruptures."
-        source="Trustmed Base de déclaration des rutpures de stock"
+        title="Déclarations de ruptures et risques de rupture de stock de médicament"
+        description="Consultez les statistiques globales sur l'historique des déclarations de rupture et de risque de rupture des  médicaments d'intérêt thérapeutique majeur."
         href="/ruptures"
         image={<OutOfStockSvg />}
       />
@@ -28,7 +27,6 @@ export const MoreInfoSection = () => (
         className="mb-8"
         title="Déclarations de cas d’effets indésirables"
         description="Consultez les données de déclaration d’effets indésirables reçues, utilisées par les agents de l’ANSM dans le cadre de la surveillance des effets indésirables tout au long de la vie du médicament."
-        source="Base de données publiques du médicament, Système National des Données de Santé (CNAM)"
         href="/globaldec"
         image={<SickPersonSvg className="h-full" />}
       />
