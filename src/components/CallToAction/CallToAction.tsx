79f809fe8d979adc18bf4a47f5d5531b85353f0b
choucry13 <beroucry@yahoo.fr>
2022-12-01 12:31:15 +0100

    DATS-243 feat(components): modify strategy Cta Link

--- src/components/CallToAction/CallToAction.tsx
+++ src/components/CallToAction/CallToAction.tsx
@@ -22,31 +22,6 @@ type CallToActionButtonProps = CallToActionBaseProps &
 
 type CallToActionProps = CallToActionButtonProps | CallToActionLinkProps;
 
-const LinkApp = ({
-  href,
-  theme,
-  className,
-  children,
-}: { href: string; theme: string; children: ReactNode } & HTMLAttributes<HTMLAnchorElement>) => (
-  <Link href={href}>
-    <a
-      className={classnames(
-        {
-          'underline border-primary text-primary hover:decoration-primary focus:decoration-primary':
-            theme === 'primary',
-          'text-secondary hover:decoration-secondary focus:decoration-secondary':
-            theme === 'secondary',
-          'text-grey hover:decoration-grey focus:decoration-grey': theme === 'grey',
-        },
-        'hover:font-medium focus:font-medium',
-        className
-      )}
-    >
-      {children}
-    </a>
-  </Link>
-);
-
 export const CallToAction = (props: CallToActionProps) => {
   const { theme = 'primary' } = props;
 
@@ -81,35 +56,24 @@ export const CallToAction = (props: CallToActionProps) => {
   }
 
   const { href, children, className, ...rest } = props;
-  if (
-    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/.exec(
-      href
-    )
-  ) {
-    return (
-      <LinkApp href={href} className={className} theme={theme} {...rest}>
-        {children}
-      </LinkApp>
-    );
-  }
-
   return (
-    <a
-      className={classnames(
-        {
-          'underline border-primary text-primary hover:decoration-primary focus:decoration-primary':
-            theme === 'primary',
-          'text-secondary hover:decoration-secondary focus:decoration-secondary':
-            theme === 'secondary',
-          'text-grey hover:decoration-grey focus:decoration-grey': theme === 'grey',
-        },
-        'hover:font-medium focus:font-medium',
-        className
-      )}
-      href={href}
-    >
-      {children}
-    </a>
+    <Link {...rest} href={href}>
+      <a
+        className={classnames(
+          {
+            'underline border-primary text-primary hover:decoration-primary focus:decoration-primary':
+              theme === 'primary',
+            'text-secondary hover:decoration-secondary focus:decoration-secondary':
+              theme === 'secondary',
+            'text-grey hover:decoration-grey focus:decoration-grey': theme === 'grey',
+          },
+          'hover:font-medium focus:font-medium',
+          className
+        )}
+      >
+        {children}
+      </a>
+    </Link>
   );
 };
 
