84cfd716c72983dacbd40a3c822416a4884898ad
choucry13 <beroucry@yahoo.fr>
2022-12-01 13:27:44 +0100

    DATS-238 feat(pages): modify block faq landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { SearchSection } from '../componentsPages/Landing/SearchSection/SearchSection';
 import { IntroSection } from '../componentsPages/Landing/IntroSection/IntroSection';
 import { MoreInfoSection } from '../componentsPages/Landing/MoreInfoSection/MoreInfoSection';
+import { FaqSection } from '../componentsPages/Landing/FaqSection/FaqSection';
 
 const HomePage = () => (
   <>
     <IntroSection />
     <SearchSection />
     <MoreInfoSection />
+    <FaqSection />
   </>
 );
 
