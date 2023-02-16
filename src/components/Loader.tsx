da97924e059cf55e9244564aa3effaa2e930f460
nicolas giraudo <giraudo.nicolas13@gmail.com>
2023-02-16 14:42:32 +0400

    fix: loader a bit upper

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,13 +4,15 @@ import GelOrange from '../assets/loader/Gélule-120-orange.svg';
 import GelGreen from '../assets/loader/Gélule-120-green.svg';
 import GelViolet from '../assets/loader/Gélule-120-violet.svg';
 import GelBlue from '../assets/loader/Gélule-120-blue.svg';
+import { useLayoutContext } from '../contexts/LayoutContext';
 
 export const Loader = () => {
+  const { navBarHeight } = useLayoutContext();
   const pictos = [GelOrange, GelGreen, GelViolet, GelBlue];
   const selected = Math.floor(Math.random() * pictos.length);
 
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="flex justify-center items-center h-screen" style={{ marginTop: -navBarHeight }}>
       <div className="flex flex-col items-center">
         {React.createElement(pictos[selected], {
           className: 'animate-bounce w-20 h-20',
