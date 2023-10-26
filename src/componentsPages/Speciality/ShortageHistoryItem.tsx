e87a0a04ef756d0398ab53937593e20a2d5906ef
ChristopherLELEU <christopher.leleu@ansm.sante.fr>
2023-10-26 10:17:05 +0200

    DATS-397: tableau histo RS, label Medicament

--- src/componentsPages/Speciality/ShortageHistoryItem.tsx
+++ src/componentsPages/Speciality/ShortageHistoryItem.tsx
@@ -13,7 +13,7 @@ export const ShortageHistoryItem = ({
 }) => (
   <div className="flex flex-col gap-1">
     <div className="flex items-center">
-      <span className="flex-1 uppercase text-sm">Présentation du médicament</span>
+      <span className="flex-1 uppercase text-sm">Médicament</span>
       <span className="flex-1">{shortageItem.cis?.name}</span>
     </div>
     <div className="flex items-center">
