ebf807e2b0631e0de072acd656e4e005825cac95
MonKeySVG <play95100@gmail.com>
2024-05-28 15:17:15 +0200

    Composant liste de médicaments fonctionnel

--- src/components/MedicineList.tsx
+++ src/components/MedicineList.tsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+import ArrowFirst from '../assets/pictos/icons/pagination/first.svg';
+import ArrowPrevious from '../assets/pictos/icons/pagination/previous.svg';
+import ArrowNext from '../assets/pictos/icons/pagination/next.svg';
+import ArrowLast from '../assets/pictos/icons/pagination/last.svg';
 
 interface Medicine {
     id: number;
     name: string;
-    // Add other medicine properties here
+    // Ajouter d'autres propriétés ici conformément à la base de données
 }
 
 interface MedicineListProps {
     medicines: Medicine[];
 }
 
-const MedicineList: React.FC<MedicineListProps> = ({ medicines }) => {
+const MedicineList: React.FC<MedicineListProps> = ({medicines}) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage, setItemsPerPage] = useState(10);
 
@@ -30,22 +34,43 @@ const MedicineList: React.FC<MedicineListProps> = ({ medicines }) => {
     const totalPages = Math.ceil(medicines.length / itemsPerPage);
 
     return (
-        <div className="bg-white rounded-lg p-4 shadow-md">
-            <h2>Total Medicines: {medicines.length}</h2>
-            {currentItems.map((medicine) => (
-                <div key={medicine.id}>{medicine.name}</div>
-            ))}
-            <div>
-                <select value={itemsPerPage} onChange={handleChangeItemsPerPage}>
-                    <option value={10}>10</option>
-                    <option value={20}>20</option>
-                    <option value={50}>50</option>
-                </select>
-                {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
-                    <button key={pageNumber} onClick={() => handleChangePage(pageNumber)}>
-                        {pageNumber}
-                    </button>
+        <div className="bg-white rounded-lg p-4 shadow">
+            <p className="text-dark-green px-4">{medicines.length} médicaments identifiés</p>
+            <div className="bg-white rounded-lg p-4 my-8 shadow">
+                {currentItems.map((medicine, index) => (
+                    <div key={medicine.id} className={`${index < currentItems.length - 1 ? 'border-b border-grey-50' : ''} py-2`}>
+                        {medicine.name}
+                    </div>
                 ))}
+            </div>
+            <div className="flex justify-between">
+                <div className="flex">
+                    <p>Nombre de lignes par page : </p>
+                    <select value={itemsPerPage} onChange={handleChangeItemsPerPage}>
+                        <option value={5}>5</option>
+                        <option value={10}>10</option>
+                        <option value={20}>20</option>
+                        <option value={50}>50</option>
+                    </select>
+                </div>
+
+                <div className="flex justify-between">
+                    <button onClick={() => handleChangePage(1)} disabled={currentPage === 1}>
+                        <ArrowFirst style={{width: '24px'}}/>
+                    </button>
+                    <button onClick={() => handleChangePage(currentPage - 1)} disabled={currentPage === 1}>
+                        <ArrowPrevious style={{width: '24px'}}/>
+                    </button>
+                    <span>
+                        {currentPage}/{totalPages}
+                    </span>
+                    <button onClick={() => handleChangePage(currentPage + 1)} disabled={currentPage === totalPages}>
+                        <ArrowNext style={{width: '24px'}}/>
+                    </button>
+                    <button onClick={() => handleChangePage(totalPages)} disabled={currentPage === totalPages}>
+                        <ArrowLast style={{width: '24px'}}/>
+                    </button>
+                </div>
             </div>
         </div>
     );
